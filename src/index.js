f5fc99c423317610842bad857cd8a08aa4a1a4c0
Marko <m4rko2001@gmail.com>
2025-12-17 09:36:26 +0100

    added default font

--- src/index.js
+++ src/index.js
@@ -5,22 +5,12 @@ Blits.Launch(App, 'app', {
   w: 1920,
   h: 1080,
   debugLevel: 1,
-  defaultFont: 'lato',
+  defaultFont: 'bebas',
   fonts: [
     {
-      family: 'lato',
+      family: 'bebas',
       type: 'msdf',
-      file: 'fonts/Lato-Regular.ttf',
-    },
-    {
-      family: 'raleway',
-      type: 'msdf',
-      file: 'fonts/Raleway-ExtraBold.ttf',
-    },
-    {
-      family: 'opensans',
-      type: 'web',
-      file: 'fonts/OpenSans-Medium.ttf',
+      file: 'fonts/BebasNeue-Regular.ttf',
     },
   ],
 })
